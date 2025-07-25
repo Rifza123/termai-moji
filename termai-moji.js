3ae1e81d8fe6faed7ba381688184255d12778f3c
Azfir <91201322+Rifza123@users.noreply.github.com>
2025-07-25 16:18:13 +0700

    Update termai-moji.js
    Support auto size

--- termai-moji.js
+++ termai-moji.js
@@ -1,100 +1,117 @@
 function TermaiMoji() {
-	const emojiMap = {};
-	const emojiRegex = /\p{Emoji_Presentation}|\p{Extended_Pictographic}/gu;
+    const emojiMap = {};
+    const emojiRegex = /\p{Emoji_Presentation}|\p{Extended_Pictographic}/gu;
 
-	function toEmojiFileName(emoji) {
-		const codePoints = Array.from(emoji).map((c) =>
-			c.codePointAt(0).toString(16)
-		);
-		return "emoji_u" + codePoints.join("_") + ".png";
-	}
+    function toEmojiFileName(emoji) {
+        const codePoints = Array.from(emoji).map((c) =>
+            c.codePointAt(0).toString(16)
+        );
+        return "emoji_u" + codePoints.join("_") + ".png";
+    }
 
-	return {
-		register(emoji, {
-			src
-		}) {
-			emojiMap[emoji] = src;
-		},
+    return {
+        register(emoji, {
+            src
+        }) {
+            emojiMap[emoji] = src;
+        },
 
-		init(base = "https://c.termai.cc/emojis/ios/") {
-			const text = document.body.innerText;
-			const found = text.match(emojiRegex) || [];
+        init(base = "https://c.termai.cc/emojis/ios/") {
+            const text = document.body.innerText;
+            const found = text.match(emojiRegex) || [];
 
-			found.forEach((emoji) => {
-				if (!(emoji in emojiMap)) {
-					const fileName = toEmojiFileName(emoji);
-					this.register(emoji, {
-						src: base + fileName
-					});
-				}
-			});
-		},
+            found.forEach((emoji) => {
+                if (!(emoji in emojiMap)) {
+                    const fileName = toEmojiFileName(emoji);
+                    this.register(emoji, {
+                        src: base + fileName
+                    });
+                }
+            });
+        },
 
-		parse(element, options = {}) {
-			if (!element) return;
+        parse(element, options = {}) {
+            if (!element) return;
 
-			const walk = document.createTreeWalker(
-				element,
-				NodeFilter.SHOW_TEXT,
-				null,
-				false
-			);
-			const nodes = [];
+         
+            let emojiWidth, emojiHeight;
+            
+            if (options.width && options.height) {
+                emojiWidth = options.width;
+                emojiHeight = options.height;
+            } else if (options.size) {
+                emojiWidth = emojiHeight = options.size;
+            } else {
+              
+                const computedStyle = window.getComputedStyle(element);
+                const fontSize = parseInt(computedStyle.fontSize);
+                const autoSize = Math.floor(fontSize * 0.95) + "px"; 
+                emojiWidth = emojiHeight = autoSize;
+            }
 
-			while (walk.nextNode()) {
-				nodes.push(walk.currentNode);
-			}
+            const walk = document.createTreeWalker(
+                element,
+                NodeFilter.SHOW_TEXT,
+                null,
+                false
+            );
+            const nodes = [];
 
-			nodes.forEach((textNode) => {
-				const parent = textNode.parentNode;
-				const text = textNode.nodeValue;
+            while (walk.nextNode()) {
+                nodes.push(walk.currentNode);
+            }
 
-				let matched = false;
-				const fragments = document.createDocumentFragment();
+            nodes.forEach((textNode) => {
+                const parent = textNode.parentNode;
+                const text = textNode.nodeValue;
 
-				let lastIndex = 0;
-				for (let i = 0; i < text.length;) {
-					const char = text[i];
-					const codePoint = text.codePointAt(i);
-					const emoji = String.fromCodePoint(codePoint);
-					const emojiLength = emoji.length;
+                let matched = false;
+                const fragments = document.createDocumentFragment();
 
-					if (emojiMap[emoji]) {
-						if (i > lastIndex) {
-							fragments.appendChild(
-								document.createTextNode(text.slice(lastIndex, i))
-							);
-						}
+                let lastIndex = 0;
+                for (let i = 0; i < text.length;) {
+                    const char = text[i];
+                    const codePoint = text.codePointAt(i);
+                    const emoji = String.fromCodePoint(codePoint);
+                    const emojiLength = emoji.length;
 
-						const img = document.createElement("img");
-						img.src = emojiMap[emoji];
-						img.alt = emoji;
-						img.style.width = options.width || "1em";;
-						img.style.height = options.height || "1em";;
-						img.style.verticalAlign = options.verticalAlign || "middle";
-						img.style.display = options.display || "inline";
-						img.style.objectFit = options.objectFit || "contain";
-						img.style.maxHeight = options.maxHeight || "10em";
-						img.style.marginRight = "2px"
-						
-						fragments.appendChild(img);
-						matched = true;
-						i += emojiLength;
-						lastIndex = i;
-					} else {
-						i += emoji.length;
-					}
-				}
+                    if (emojiMap[emoji]) {
+                        if (i > lastIndex) {
+                            fragments.appendChild(
+                                document.createTextNode(text.slice(lastIndex, i))
+                            );
+                        }
 
-				if (matched) {
-					if (lastIndex < text.length) {
-						fragments.appendChild(
-							document.createTextNode(text.slice(lastIndex))
-						);
-					}
-					parent.replaceChild(fragments, textNode);
-				}
-			});
-		},
-	};
+                        const img = document.createElement("img");
+                        img.src = emojiMap[emoji];
+                        img.alt = emoji;
+                        img.style.width = emojiWidth;
+                        img.style.height = emojiHeight;
+                        img.style.verticalAlign = options.verticalAlign || "text-bottom";
+                        img.style.display = options.display || "inline-block";
+                        img.style.objectFit = "contain";
+                      
+                        if (options.marginRight) img.style.marginRight = options.marginRight;
+                        if (options.marginLeft) img.style.marginLeft = options.marginLeft;
+
+                        fragments.appendChild(img);
+                        matched = true;
+                        i += emojiLength;
+                        lastIndex = i;
+                    } else {
+                        i += emoji.length;
+                    }
+                }
+
+                if (matched) {
+                    if (lastIndex < text.length) {
+                        fragments.appendChild(
+                            document.createTextNode(text.slice(lastIndex))
+                        );
+                    }
+                    parent.replaceChild(fragments, textNode);
+                }
+            });
+        },
+    };
 }
